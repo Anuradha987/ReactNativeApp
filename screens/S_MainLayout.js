509e44d4352493c8e9c95a3ea02f92fc15862fe4
lojithv <lojithvinsuka1998@gmail.com>
2022-05-19 20:30:57 +0530

    unnecessary loading

--- screens/S_MainLayout.js
+++ screens/S_MainLayout.js
@@ -25,19 +25,6 @@ const TabButton = ({label, icon, isFocused, onPress, outerContainerStyle, innerC
    }, []);
   
     return(
-        (!loaded)?
-        (
-            <View
-              style={{
-                flex: 4,
-                backgroundColor: 'rgba(21,31,40,1)',
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
-              {/* https://github.com/n4kz/react-native-indicators */}
-              <ActivityIndicator size="large" />
-            </View>
-          ):
         (<TouchableWithoutFeedback onPress={onPress}>
             <Animated.View style={[{flex:1, alignItems: 'center', justifyContent: 'center'}, outerContainerStyle]}>
 
